3b6f20d9047a9543bd404c92701182a4d7dd7812
Prabin Shrestha <shrestha.prabinman@gmail.com>
2024-08-31 22:10:27 +0545

    implemented read write function for database

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #include "include/list.h"
+#include "include/database.h"
 
 int main(int arg_count, char *args[]) {
 
-    if(arg_count > 1) {
-        List simpleList;
+    List simpleList;
+    Database data;
 
+    if(arg_count > 1) {
         simpleList.name = string(args[1]);
         simpleList.print_menu();
+        data.write(simpleList.list);
+        data.read();
     }
     else {
         cout << "Username not supplied... exiting the program" << endl;
     }
+
     return 0;
 }
 
